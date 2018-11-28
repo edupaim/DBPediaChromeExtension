f91a779a5fde32c02052f06c9e2d5bc6f93c38f8
Eduardo Paim <eduardosantos@kofre.com.br>
2018-11-28 19:09:36 -0300

    funcionado

--- popup.js
+++ popup.js
@@ -1,53 +1,99 @@
 chrome.tabs.executeScript({
     code: "window.getSelection().toString();"
 }, function (selection) {
-    chrome.extension.getBackgroundPage().console.log("selection[0]");
-    //chrome.extension.getBackgroundPage().console.log(selection[0]);
-    document.getElementById("output").innerHTML = selection[0];
-});
+    document.getElementById("output").innerHTML = selection && selection[0];
 
-SPARQL = function(o) {
-    this.query = function(q) {
-      return $.ajax({
-        url: o.endpoint,
-        accepts: {json: "application/sparql-results+json"},
-        data: {query: q, apikey: o.apikey},
-        dataType: "json"
-      });
-    };
-  };
-var bioportal = new SPARQL({
-    apikey: "YOUR-API-KEY-HERE",
-    endpoint: "http://sparql.bioontology.org/sparql/"
-});
-var query_string = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#>\n\
-                    SELECT ?ont ?name ?acr \n\
-                    WHERE { \n\
-                       ?ont a omv:Ontology .  \n\
-                       ?ont omv:acronym ?acr.  \n\
-                       ?ont omv:name ?name .\n\
-                       FILTER (str(?acr)='SNOMEDCT')\n\
-                    }";
-function onFailure(xhr, status) {
-    document.getElementById("result").innerHTML = status + " (See console.)";
-    console.log("error");
-    console.log(xhr);
-}
-function onSuccess(json) {
-    console.log("ok!")
-    var html = "<table border='1'>";
-    for (var b in json.results.bindings) {
-        html += "<tr>";
-        for (var x in json.head.vars) {
-            var value = json.results.bindings[b][json.head.vars[x]];
-            if (value.type == "uri")
-                html += "<td><a href='" + value.value + "'>" + value.value + "</a></td>";
-            else
-                html += "<td>" + value.value + "</td>";
-        }
-        html += "</tr>";
+
+    //async request to the url -> print the result
+    function httpGetAsync(theUrl) {
+        $.ajax({
+            url: theUrl,
+            data: {
+                format: 'json'
+            },
+            error: function () {
+                document.getElementById("output").innerHTML = status + " (See console.)";
+            },
+            dataType: 'json',
+            success: function (json) {
+                var html = "<table border='1'>";
+                for (var b in json.results.bindings) {
+                    html += "<tr>";
+                    for (var x in json.head.vars) {
+                        var value = json.results.bindings[b][json.head.vars[x]];
+                        if (value.type == "uri")
+                            html += "<td><a href='" + value.value + "'>" + value.value + "</a></td>";
+                        else
+                            html += "<td>" + value.value + "</td>";
+                    }
+                    html += "</tr>";
+                }
+                html += "</table>";
+                document.getElementById("output").innerHTML = html;
+            },
+            type: 'GET'
+        });
     }
-    html += "</table>";
-    document.getElementById("result").innerHTML = html;
-}
-bioportal.query(query_string).done(onSuccess).error(onFailure);
+    //sparql query
+    var query = [
+        "PREFIX dbo: <http://dbpedia.org/ontology/>",
+        "SELECT ?album ?artist WHERE {",
+        "?album dbo:artist ?artist .",
+        "} LIMIT 10"
+    ].join(" ");
+    //url for the query
+    var url = "http://dbpedia.org/sparql";
+    var queryUrl = url + "?query=" + encodeURIComponent(query);
+    //query call
+    httpGetAsync(queryUrl);
+
+
+    /*SPARQL = function (o) {
+        this.apiKey = o.apikey;
+        this.endpoint = o.endpoint;
+        this.query = function (q) {
+            chrome.extension.getBackgroundPage().console.log("Ajax!");
+            return $.ajax({
+                url: this.endpoint,
+                accepts: { json: "application/sparql-results+json" },
+                data: { query: q, apikey: this.apiKey },
+                dataType: "json"
+            });
+        };
+    };
+    var bioportal = new SPARQL({
+        apikey: "YOUR-API-KEY-HERE",
+        endpoint: "http://sparql.bioontology.org/sparql/"
+    });
+    var query_string = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#>\n\
+                        SELECT ?ont ?name ?acr \n\
+                        WHERE { \n\
+                           ?ont a omv:Ontology .  \n\
+                           ?ont omv:acronym ?acr.  \n\
+                           ?ont omv:name ?name .\n\
+                           FILTER (str(?acr)='SNOMEDCT')\n\
+                        }";
+    function onFailure(xhr, status) {
+        document.getElementById("output").innerHTML = status + " (See console.)";
+    }
+    function onSuccess(json) {
+        var html = "<table border='1'>";
+        for (var b in json.results.bindings) {
+            html += "<tr>";
+            for (var x in json.head.vars) {
+                var value = json.results.bindings[b][json.head.vars[x]];
+                if (value.type == "uri")
+                    html += "<td><a href='" + value.value + "'>" + value.value + "</a></td>";
+                else
+                    html += "<td>" + value.value + "</td>";
+            }
+            html += "</tr>";
+        }
+        html += "</table>";
+        document.getElementById("output").innerHTML = html;
+    }
+    chrome.extension.getBackgroundPage().console.log(bioportal);
+    bioportal.query(query_string)
+        .done(onSuccess)
+        .fail(onFailure);*/
+});
